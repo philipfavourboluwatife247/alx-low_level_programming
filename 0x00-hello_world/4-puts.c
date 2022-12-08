3abf1632c0c49dcc3717f77c388f9d2938775162
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-08 12:51:18 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,10 +9,7 @@
  *
  *     */
 
-int main(void)
-
-{
-	    puts("\"Programming is like building a multilingual puzzle\n");
-	    return (0);
-
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return (0);
 }
