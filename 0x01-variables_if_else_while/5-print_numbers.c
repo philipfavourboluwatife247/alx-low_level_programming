ff971be5759d899a9966e711c90c2b1e8b653b24
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-09 13:08:57 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0, followed by a new line
+ *
+ * Return: return type is 0
+ */
+int main(void)
+{
+	char binaryNum[10] = "0123456789";
+	printf("%s\n", binaryNum);
+
+	return (0);
+
+}
