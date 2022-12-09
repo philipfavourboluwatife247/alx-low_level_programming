d9ae21a2b45edb27b47211e239483b550d1d2376
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-09 12:27:51 -0800

    ALPHA alpha game

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,16 @@ int main(void)
 	char lowerAlph[26] = "abcdefghijklmnopqrstuvwxyz";
 	char upperAlph[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+	int p;
 
 	for (i = 0; i < 26; i++)
 	{
 		putchar(lowerAlph[i]);
-		putchar(upperAlph[i]);
+	}
+	
+	for (p = 0; p < 26; p++)
+	{
+		putchar(upperAlph[p]);
 	}
 	putchar('\n');
 	return (0);
