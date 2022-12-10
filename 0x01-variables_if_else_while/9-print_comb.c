a70e6ed870dfc34f81861e0ac33620d0ed2fe2ee
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-09 20:35:20 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	
+	}
+	putchar('\n');
+	return (0);
+
+}
