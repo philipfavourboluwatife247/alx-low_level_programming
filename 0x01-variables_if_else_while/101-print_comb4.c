7c0ef2670bc56ff841971c63d93b32b2f5f315be
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-09 22:14:40 -0800

    Print 3 comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -6,15 +8,29 @@
  */
 int main(void)
 {
-	int a;
+	int a, b, c;
 
-	for (a = 0; a < 1000; a++)
+	for (a = '0'; a < '9'; a++)
 	{
-		putchar(a + '0');
-		if (a < 999)
+		for (b = a + 1; b <= '9'; b++)
 		{
-			putchar(',');
-			putchar(' ');
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if ((b != a) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+						if (a == '7' && b == '8')
+						{	
+							continue;
+							putchar(',');
+							putchar(' ');
+						}
+				}
+			}
+
+		
 		}
 	}
 	putchar('\n');
