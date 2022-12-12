61ceea8eeebefaff3a2c11f010ea631c398a34a9
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-12 10:40:03 -0800

    prints the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
- * main - prints alphabet in lower case and then upper case followed by a new line
+ * main - Entry point
  *
  * Return: return type is 0
  */
 int main(void)
 {
 	char lowerAlph[26] = "abcdefghijklmnopqrstuvwxyz";
+
 	char upperAlph[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 	int i;
+
 	int p;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(lowerAlph[i]);
+	putchar(lowerAlph[i]);
 	}
 	
 	for (p = 0; p < 26; p++)
 	{
-		putchar(upperAlph[p]);
+	putchar(upperAlph[p]);
 	}
 	putchar('\n');
 	return (0);
-
 }
