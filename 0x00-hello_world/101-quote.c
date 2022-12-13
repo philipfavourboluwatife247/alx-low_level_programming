d02f929688b2d47f85df5f01a0e9aeafeaa5f1dc
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-12 21:31:59 -0800

    A quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
-{ #include <stdio.h>
-/**                                                                                                                            * main - Entry point 
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
  *
- * Return: Always 0 (Success)                                                                                
- */  
-
-
-	int main(void) 
+ * Return: return value is 1
+ */
+int main(void)
 {
-       	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-
-
-
-		return (1);
-
-	} }
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
