3c84bca8dbb69623fb4066e41ed286547eaa03d2
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-13 14:53:00 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * This code is written by Boluwatife Philip
+ * Return: return type is 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Solution to my tasks on function and nested loops
