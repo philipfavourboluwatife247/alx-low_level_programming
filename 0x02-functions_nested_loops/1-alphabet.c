0c1fd79317aae45910d12d976a7700c53bdbb6c5
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-13 15:42:15 -0800

    Prints alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+
+	_putchar('\n');
+}
