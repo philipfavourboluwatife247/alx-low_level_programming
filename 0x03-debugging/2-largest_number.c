bd21f0ed10ffc525a0d86a47a5dfdac3e03eac51
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-14 13:08:55 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest;
 
 if (a > b && a > c)
 {
-largest = a;
+	largest = a;
 }
 else if (b > a && b > c)
 {
-largest = b;
+	largest = b;
 }
 else
 {
-largest = c;
+	largest = c;
 }
 
 return (largest);
