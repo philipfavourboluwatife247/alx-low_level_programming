c2614ff1383c96789977464da1ab2c073e1c7930
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-15 05:53:49 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+/**
+ * print_square - Prints a squareusing the character #.
+ * @size: The size of the square.
+ */
+void print_square(int size)
+{
+	int length;
+
+	int breadth;
+
+	if (size > 0)
+	{
+		for (length = 0; length < size; length++)
+		{
+			for (breadth = 0; breadth < size; breadth++)
+				_putchar('#');
+			if (length == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
