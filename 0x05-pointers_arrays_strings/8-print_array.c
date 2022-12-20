f51373fd3753e986f953981c1a89a380929d075b
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-19 17:16:39 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+/*
+ * File: 8-print_array.c
+ * Auth: Philip Favour B.
+ * Date: 20th December, 2022.
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints an inputted number of elements
+ *               of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
