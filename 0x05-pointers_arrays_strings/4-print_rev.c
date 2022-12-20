56e56e4ba2e477fba243d97aef65179d1bfadc0c
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2022-12-19 20:26:46 -0800

    Updates

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,14 @@
  */
 void print_rev(char *s)
 {
+	
 	int len = 0, index;
 
 	while (s[index++])
 		len++;
 
 	for (index = len - 1; index >= 0; index--)
+		
 		_putchar(s[index]);
 
 	_putchar('\n');
