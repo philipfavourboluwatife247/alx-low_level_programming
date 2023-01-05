d7bcd03ecdcb593e919f29403568f0a17dab4a45
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2023-01-04 17:00:06 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+/*
+ * Auth: Philip Favour B.
+ * File: 4-pow_recursion.c
+ */
+#include "main.h"
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The number to be raised.
+ * @y: The power.
+ *
+ * Return: The value of x raised to the power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
