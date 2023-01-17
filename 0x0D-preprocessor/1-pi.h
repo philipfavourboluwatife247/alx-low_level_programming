b2e0787e2f2e9981e2fa3e9f7d9764684ebdaae2
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2023-01-16 16:46:20 -0800

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * Auth: Philip Favour B.
+ * File: 1-pi.h
+ * Desc: This is a header file
+ *	for a macro named PI
+ *	with a token 3.14159265359
+ */
+
+#define PI 3.14159265359
+
+#endif
