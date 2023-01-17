290137e6d8da823858f1b33b0fc18082ffa055b9
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2023-01-16 16:39:38 -0800

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -5,8 +5,8 @@
  * Auth: Philip Favour Boluwatife
  * File: 0-object_like_macro.h
  * Desc: This header file contains:
- * 	a defined macro named SIZE
- * 	with an abbreviation of 1024.
+ *	a defined macro named SIZE
+ *	with an abbreviation of 1024.
  */
 
 #define SIZE 1024
