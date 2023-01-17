a19277ce605d87e941379bf22fb63be9dec7d0d0
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2023-01-16 16:23:13 -0800

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * Auth: Philip Favour Boluwatife
+ * File: 0-object_like_macro.h
+ * Desc: This header file contains:
+ * 	a defined macro named SIZE
+ * 	with an abbreviation of 1024.
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,5 @@
+This directory contains task on preprocessing to check my understanding of the general objectives which are:
+
+*What are macros and how to use them
+**What are the most common predefined macros
+***How to include guard your header files
